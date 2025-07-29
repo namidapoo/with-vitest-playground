67e7be72c42fd6294ef9b0c4410d0a949fdfab34
namidapoo <83203852+ve1997@users.noreply.github.com>
2025-07-29 13:06:02 +0900

    fix(cdk-build-test): format

--- apps/cdk-build-test/lib/cdk-build-test-stack.ts
+++ apps/cdk-build-test/lib/cdk-build-test-stack.ts
@@ -1,16 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import type { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkBuildTestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	// biome-ignore lint/complexity/noUselessConstructor: <Provisional Measures>
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
+		// The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkBuildTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+		// example resource
+		// const queue = new sqs.Queue(this, 'CdkBuildTestQueue', {
+		//   visibilityTimeout: cdk.Duration.seconds(300)
+		// });
+	}
 }
